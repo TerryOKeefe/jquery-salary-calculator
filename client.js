9492bf7a3f8d69af3581e5e613ecf3ba0c35dc6a
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-05-14 15:05:43 -0500

    sourced files into html

---
+++ client.js
@@ -0,0 +1,11 @@
+console.log('js loaded');
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    // console log to test source in
+    console.log('jQuery sourced');
+}
+
+
+console.log('js ended');
